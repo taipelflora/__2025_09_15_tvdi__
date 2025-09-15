6e1e9d43d2988812156f3acc548942ec77121c5f
徐國堂 <roberthsu2003@gmail.com>
2025-09-15 16:26:48 +0800

    modify

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return '<h1 style="color:red">您好! Flaks!</h1>'
 
+@app.route("/name")
+def name():
+    return '<h1>Hello! Robert</h1>'
+
 if __name__ == "__main__":
     app.run(debug=True)

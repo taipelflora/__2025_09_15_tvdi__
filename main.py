cee009505c38946b9ced71cec71d8cd295835e44
徐國堂 <roberthsu2003@gmail.com>
2025-09-15 15:44:08 +0800

    modify

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from 2025-09-15-tvdi!")
+#todo:請建立一個Flask應用程式，並在根路由('/')上顯示"Hello, Flask!"訊息。
+from flask import Flask
 
+app = Flask(__name__)
 
-if __name__ == "__main__":
-    main()
+@app.route('/')
+def hello():
+    return 'Hello, Flask!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+

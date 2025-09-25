91b3cabda1b86dfa0bd39ddd620e21d81943b923
徐國堂 <roberthsu2003@gmail.com>
2025-09-25 15:04:07 +0800

    modify

--- lesson3/lesson3_3.py
+++ lesson3/lesson3_3.py
@@ -1,25 +1,29 @@
 import requests
 
-url = 'https://data.ntpc.gov.tw/api/datasets/010e5b15-3823-4b20-b401-b1cf000550c5/json?page=0&size=1000'
+def download_youbike_data():
+    url = 'https://data.ntpc.gov.tw/api/datasets/010e5b15-3823-4b20-b401-b1cf000550c5/json?page=0&size=1000'
 
-try:
-    response = requests.get(url)
-    response.raise_for_status()
     try:
-        data = response.json()
-    except requests.exceptions.JSONDecodeError as jsonError:
-        print(f"發生轉換格式錯誤:jsonError")
-except requests.exceptions.HTTPError as err_http:
-    print(f"發生 HTTP 錯誤: {err_http}")
-except requests.exceptions.ConnectionError as err_conn:
-    print(f"發生連線錯誤 (例如 DNS 查詢失敗、連線被拒): {err_conn}")
-except requests.exceptions.Timeout as err_timeout:
-    print(f"請求超時: {err_timeout}")
-except requests.exceptions.RequestException as err:
-    # 這是所有 requests 例外的父類別，可以用來捕捉其他未預期的錯誤
-    print(f"發生未預期的請求錯誤: {err}")
-else:
-    print(f"沒有出錯:\n{data}")
+        response = requests.get(url)
+        response.raise_for_status()
+        try:
+            data = response.json()
+        except requests.exceptions.JSONDecodeError as jsonError:
+            print(f"發生轉換格式錯誤:jsonError")
+    except requests.exceptions.HTTPError as err_http:
+        print(f"發生 HTTP 錯誤: {err_http}")
+    except requests.exceptions.ConnectionError as err_conn:
+        print(f"發生連線錯誤 (例如 DNS 查詢失敗、連線被拒): {err_conn}")
+    except requests.exceptions.Timeout as err_timeout:
+        print(f"請求超時: {err_timeout}")
+    except requests.exceptions.RequestException as err:
+        # 這是所有 requests 例外的父類別，可以用來捕捉其他未預期的錯誤
+        print(f"發生未預期的請求錯誤: {err}")
+    else:
+        print(f"沒有出錯:\n{data}")
+
+if __name__ == "__main__":
+    download_youbike_data()
 
 
 

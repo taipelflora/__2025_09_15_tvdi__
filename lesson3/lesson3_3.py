3007398bb08566a886e7ed9e1a06f1963b7b911d
徐國堂 <roberthsu2003@gmail.com>
2025-09-25 13:51:31 +0800

    modify

--- lesson3/lesson3_3.py
+++ lesson3/lesson3_3.py
@@ -1,1 +1,11 @@
 import requests
+
+url = 'https://data.ntpc.gov.tw/api/datasets/010e5b15-3823-4b20-b401-b1cf000550c5/json?page=0&size=1000'
+
+response = requests.get(url)
+#print(type(response))
+
+if response.status_code == 200:
+    print("下載成功")
+else:
+    print("下載失敗")

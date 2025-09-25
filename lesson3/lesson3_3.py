f1062dfe04713323d93a675caecd7d2f72d9f382
徐國堂 <roberthsu2003@gmail.com>
2025-09-25 16:08:31 +0800

    modify

--- lesson3/lesson3_3.py
+++ lesson3/lesson3_3.py
@@ -4,7 +4,13 @@ def main():
     try:
         data = tools.download_youbike_data()
         areas = tools.get_area(data)
-        print(areas)
+        print("目前可以查詢的區域:\n")
+        for area in areas:
+            print(area,end=" ")
+        print("\n")
+        selected = input("請選擇一個區域:")
+        print(selected)
+
     except Exception as e:
         print("發生錯誤\n{e}")
     

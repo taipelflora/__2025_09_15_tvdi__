573c85557cba4ead35fe2ca921b9d8ef43c696bd
徐國堂 <roberthsu2003@gmail.com>
2025-10-01 11:48:38 +0800

    modify

--- lesson4/app.py
+++ lesson4/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask,render_template
 
-app = Flask(__name__)
+app = Flask(__name__,static_folder="static",template_folder="templates")
 
 @app.route("/")
 def index():
-    return "<h1>Hello, 徐國堂!</h1>"
+    return render_template("index.html")
 
 def main():
     """啟動應用（教學用：啟用 debug 模式）"""

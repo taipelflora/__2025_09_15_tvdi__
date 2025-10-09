bf77ea89d1fa586c47013b2b95725a9aeffd4179
徐國堂 <roberthsu2003@gmail.com>
2025-10-09 16:02:52 +0800

    modify

--- lesson6/app.py
+++ lesson6/app.py
@@ -6,9 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/machine")
-def machine():
-    return render_template("machine.html")
+@app.route("/regression")
+def regression():
+    return render_template("regression.html")
+
+@app.route("/knn")
+def knn():
+    return render_template("knn.html")
 
 @app.route("/lesson6_1")
 def lesson6_1():

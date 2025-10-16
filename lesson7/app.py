9be3c79e60a68a71a451e7a4a4a09300dae130d7
徐國堂 <roberthsu2003@gmail.com>
2025-10-16 15:54:30 +0800

    lesson7_2_2

--- lesson7/app.py
+++ lesson7/app.py
@@ -6,9 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/machine")
-def machine():
-    return render_template("machine.html") 
+@app.route("/knn")
+def knn():
+    return render_template("knn.html")
+
+@app.route("/regression")
+def regression():
+    return render_template("regression.html")
 
 def main():
     """啟動應用（教學用：啟用 debug 模式）"""
